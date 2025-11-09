f40b1de2a1f29de947d2452bf88b6f085740e60a
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2025-11-09 13:53:41 +0800

    Comment out WeChatPush package clone command
    Comment out the git clone command for luci-app-wechatpush.

--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -2,6 +2,6 @@
 
 # 自定义脚本
 # wechatpush
-git clone --depth 1 https://$github/tty228/luci-app-wechatpush package/new/luci-app-wechatpush
+# git clone --depth 1 https://$github/tty228/luci-app-wechatpush package/new/luci-app-wechatpush
 git clone --depth 1 https://$github/nikkinikki-org/OpenWrt-nikki package/new/openWrt-nikki
 git clone --depth 1 https://$github/vernesong/OpenClash package/new/openWrt-OpenClash
