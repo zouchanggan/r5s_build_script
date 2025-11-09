a3de63bb48d9267abd896f118ca497d7c949b96c
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2025-11-09 13:53:16 +0800

    Remove ArgonTheme links from footer files
    Removed references to ArgonTheme from footer files in luci-theme-argon.

--- openwrt/scripts/02-prepare_package.sh
+++ openwrt/scripts/02-prepare_package.sh
@@ -59,9 +59,6 @@ sed -i 's/0666/0644/g;s/0777/0755/g' feeds/packages/net/samba4/files/smb.conf.te
 git clone --depth 1 https://$github/jerrykuku/luci-theme-argon package/new/luci-theme-argon
 git clone --depth 1 https://$github/jerrykuku/luci-app-argon-config package/new/luci-app-argon-config
 
-sed -i '/<a href="https:\/\/github.com\/jerrykuku\/luci-theme-argon" target="_blank">ArgonTheme <%# vPKG_VERSION %><\/a> \//d' package/new/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
-sed -i '/<a href="https:\/\/github.com\/jerrykuku\/luci-theme-argon" target="_blank">ArgonTheme <%# vPKG_VERSION %><\/a> \//d' package/new/luci-theme-argon/luasrc/view/themes/argon/footer.htm
-
 # Mosdns
 git clone https://$github/sbwml/luci-app-mosdns -b v5 package/new/mosdns
 
